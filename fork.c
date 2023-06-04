1cc4723811f2877416ebfcaac3d44083cb22cce7
Dennis Johns <99817065+djohns1011@users.noreply.github.com>
2023-06-04 12:58:22 +0530

    Update fork.c

--- fork.c
+++ fork.c
@@ -3,15 +3,15 @@
 
 int main(){
     int pid, pid1, pid2;
-    if(pid ==1){
+    if(pid == -1){
         printf("ERROR in PROCESS CREATION\n");
         exit(1);
     }
-    if(pid != 0){
-        pid = getpid();
-        printf("The Parent Process ID is %d\n",pid1);
+    if(pid == 0){
+        pid1 = getpid();
+        printf("The Child Process ID is %d\n",pid1);
     }else{
         pid2 = getpid();
-        printf("The Child Process ID is %d\n",pid);
+        printf("The Parent Process ID is %d\n",pid2);
     }
 }
